b6b1e40081278a96f48fb88293265217e9279a05
woskam-dotcom <wilfred.oskam@gmail.com>
2025-05-09 14:54:42 +0200

    Fixed some errors

--- main.tf
+++ main.tf
@@ -35,10 +35,10 @@ module "blog_sg" {
   name    = "blog_new"
   
   vpc_id = data.aws_vpc.default.id
-  ingress_rules       = [http-80-tcp","https-443-tcp"]
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules        = [all-all]
+  egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
