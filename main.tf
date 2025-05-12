f831c8138750c1e24f8a1e8cfe1572db109547ef
woskam-dotcom <wilfred.oskam@gmail.com>
2025-05-12 11:02:02 +0200

    Again try

--- main.tf
+++ main.tf
@@ -29,18 +29,6 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
