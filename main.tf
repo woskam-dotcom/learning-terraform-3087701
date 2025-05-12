4a46d5788e0c1c94a5b961376f43610b49ffb0fb
woskam-dotcom <wilfred.oskam@gmail.com>
2025-05-12 10:47:21 +0200

    Trail and Error

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  azs             = ["eu-central-1a","eu-central-1b","eu-central-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
