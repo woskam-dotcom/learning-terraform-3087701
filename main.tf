3a6ab948d4f329b929bc0b4a9fa50002195dbb7e
woskam-dotcom <wilfred.oskam@gmail.com>
2025-05-12 10:36:34 +0200

    Load balancer

--- main.tf
+++ main.tf
@@ -45,6 +45,38 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name            = "blog_alb"
+  vpc_id          = "module.blog_vpc.vpc_id"
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  listeners = {
+    http-tcp-listeners = {
+      port              = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = "aws_instance.blog.id"
+    }
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
