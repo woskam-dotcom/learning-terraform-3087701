f7045b21c24af2b8f326a2c7262bc68c57b0b6f8
woskam-dotcom <wilfred.oskam@gmail.com>
2025-05-12 11:46:26 +0200

    Fix cdir error

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = var.environment.name
-  cidr = "${var.environment.network_prefix}"
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["eu-central-1a","eu-central-1b","eu-central-1c"]
   public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
