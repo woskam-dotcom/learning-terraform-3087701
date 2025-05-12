cbf1ad9c6172304fda9bc141971f00ab9f833019
woskam-dotcom <wilfred.oskam@gmail.com>
2025-05-12 10:20:02 +0200

    Fixed the Aval Zone error

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
