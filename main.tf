503c9133e827e905faa50b7b933c5daf2afd4608
woskam-dotcom <wilfred.oskam@gmail.com>
2025-05-12 10:42:32 +0200

    Target fix

--- main.tf
+++ main.tf
@@ -62,15 +62,20 @@ module "alb" {
     }
   }
 
-  target_groups = {
-    ex-instance = {
+  target_groups = [
+    {
       name_prefix      = "blog-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = "aws_instance.blog.id"
+      targets = {
+        my-target = {
+          target_id        = "aws_instance.blog.id"
+          port = 80
+        }
+      }
     }
-  }
+  ]
 
   tags = {
     Environment = "Dev"
